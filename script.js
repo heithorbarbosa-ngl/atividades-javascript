1eda0a262f775611340721003a37657ea110a5ff
heithorbarbosa-ngl <heithor_barbosa@estudante.sesisenai.org.br>
2025-09-15 20:50:04 -0300

    atividade funcoes

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("olá mundo")
+function sub(n1, n2) {
+    return n1 - n2;
+}
+
+function mul(n1, n2) {
+    return n1 * n2;
+}   
+
+function div(n1, n2) {
+    return n1 / n2;
+}   
+
+let resultado = sub(10, 5);
+console.log(resultado);

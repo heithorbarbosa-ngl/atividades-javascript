12882a64813c6414cc07521728a1a55c872102a9
heithorbarbosa-ngl <heithor_barbosa@estudante.sesisenai.org.br>
2025-09-17 21:09:15 -0300

    exercicios

---
+++ exercicio5.js
@@ -0,0 +1,6 @@
+let frutas = ["Maçã", "Banana", "Uva", "Morango", "Laranja"];
+
+console.log("Primeira fruta:", frutas[0]);
+console.log("Última fruta:", frutas[frutas.length - 1]);
+
+console.log("Total de frutas:", frutas.length);
